7c3f6c4f48dbc0cb5d8d0a2d527bd64a180ffd3f
TalelMejri <talelmejri8@gmail.com>
2023-08-31 13:32:26 +0100

    add model todo with factory

---
+++ lib/todos.dart
@@ -0,0 +1,29 @@
+
+
+
+
+import 'package:flutter/foundation.dart';
+
+class Todo{
+  int id;
+  String title;
+  String description;
+  bool complete;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.complete
+  });
+
+   factory Todo.fromJson(Map<String,dynamic> json){
+    return Todo(
+       id: json['id'], 
+       title: json['title'],
+       description: json['description'], 
+       complete: json['complete']
+      );
+   }
+   
+}
